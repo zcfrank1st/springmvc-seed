fadcb7bed6e408e5912fe3c18eb2e1d72a914bde
chao.zhang <chao.zhang@dianping.com>
2014-10-14 18:24:33 +0800

    seed

---
+++ src/main/java/com/springapp/mvc/WelcomeController.java
@@ -0,0 +1,25 @@
+package com.springapp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by zcfrank1st on 10/14/14.
+ */
+@Controller
+public class WelcomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView home () {
+        return new ModelAndView("redirect:/content/index.html");
+    }
+
+    @RequestMapping(value = "/demo", method = RequestMethod.GET)
+    @ResponseBody
+    public String demo () {
+        return "hahah";
+    }
+}
